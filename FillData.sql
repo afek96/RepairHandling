74e900579f4c0320ba49defd7d7ba0b34b8bc6fe
Adam Malcherczyk (adammal785) <adammal785@student.polsl.pl>
2019-03-16 22:06:36 +0100

    - rozszerzenie FillData.sql o 4 rekrody do tabeli ActivityType

--- FillData.sql
+++ FillData.sql
@@ -1,4 +1,4 @@
 INSERT INTO [dbo].[Role] ([RoleType],[Name]) VALUES ('ADM','Admin'),('MAN','Manager'),('WOR','Worker')
 GO
-
+INSERT [dbo].[ActivityType] ([ActivityType], [Name]) VALUES (N'LAK', N'Lakierowanie'),(N'MALOW', N'Malowanie'), (N'POLERKA', N'Polerowanie'), (N'SKREC', N'Skrêcanie')
 
